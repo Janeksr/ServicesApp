9f276532ba00b9148e1f43b908d80f98c03705c8
Janeksr <jan.dabrowski661@gmail.com>
2022-06-28 16:06:35 +0200

    Seted Reservation Table

---
+++ FeaturedServices.Data/Reservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeaturedServices.Data
+{
+    public class Reservation : BaseEntity
+    {
+        public int ClientId { get; set; }
+        public int WorkerId { get; set; }
+        public int ReservationId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
