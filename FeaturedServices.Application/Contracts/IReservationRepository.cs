9e3fc48bcb11e6090cf0a2d92d08790e7f2e1341
Janeksr <96008955+Janeksr@users.noreply.github.com>
2022-07-06 21:58:49 +0200

    Base user panel data

--- FeaturedServices.Application/Contracts/IReservationRepository.cs
+++ FeaturedServices.Application/Contracts/IReservationRepository.cs
@@ -12,6 +12,6 @@ namespace FeaturedServices.Application.Contracts
     {
         Task<IEnumerable<ReservationVM>> GetAll(int companyId, int workerId);
         Task<bool> AddResrvation(NewReservationVM newReservationVM, string userId);
-        Task GetUserReservations();
+        Task<List<UserReservations>> GetUserReservations();
     }
 }
