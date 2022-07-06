1a42dd98c5302709d3f0b7c62d47f9ea35e2f3ec
Janeksr <jan.dabrowski661@gmail.com>
2022-07-06 21:12:36 +0200

    Fully functioning backend validation

---
+++ FeaturedServices.Web/Controllers/ReservationController.cs
@@ -0,0 +1,25 @@
+﻿using FeaturedServices.Application.Contracts;
+using FeaturedServices.Common.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeaturedServices.Web.Controllers
+{
+    [Authorize(Roles = Roles.User)]
+    public class ReservationController : Controller
+    {
+        private readonly IReservationRepository _reservationRepository;
+
+        public ReservationController(IReservationRepository reservationRepository)
+        {
+            _reservationRepository = reservationRepository;
+        }
+
+
+        public async Task<IActionResult> MyResrvations()
+        {
+
+            return View();
+        }
+    }
+}
